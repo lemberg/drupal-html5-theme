cb8c577527d0568a52cc6f7ca9a6598c6a77f8f0
Andriy Ivasiv <andriy.ivasiv@lemberg.co.uk>
2015-02-09 18:24:05 +0200

    Overrode theme breadcrumbs.

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Implements hook_preprocess_page();
+ * Implements hook_preprocess_page().
  */
 function html5_preprocess_page() {
   $html5shiv = array(
@@ -15,4 +15,20 @@ function html5_preprocess_page() {
     '#suffix' => '<![endif]-->',
   );
   drupal_add_html_head($html5shiv, 'html5shiv');
+}
+
+/**
+ * Overrides theme_breadcrumb().
+ */
+function html5_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<nav class="breadcrumb">' . implode(' » ', $breadcrumb) . '</nav>';
+    return $output;
+  }
 }
