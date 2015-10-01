09a33be5e2da5e51818df042dafd7670b7f11e8c
Andriy Ivasiv <andriy.ivasiv@lemberg.co.uk>
2015-10-01 16:16:27 +0300

    Removed html5shiv.

--- template.php
+++ template.php
@@ -1,23 +1,6 @@
 <?php
 
 /**
- * Implements hook_preprocess_page().
- */
-function html5_preprocess_page() {
-  $html5shiv = array(
-    '#tag' => 'script',
-    '#value' => '',
-    '#attributes' => array(
-      'src' => file_create_url(drupal_get_path('theme', 'html5') . '/js/html5shiv.js'),
-      'type' => 'text/javascript',
-    ),
-    '#prefix' => '<!--[if lte IE 8]>',
-    '#suffix' => '<![endif]-->',
-  );
-  drupal_add_html_head($html5shiv, 'html5shiv');
-}
-
-/**
  * Overrides theme_breadcrumb().
  */
 function html5_breadcrumb($variables) {
